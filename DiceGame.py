6a3a7d3938c097a5697b8703c2a34dadc78812f4
s1300115 <s1300115@u-aizu.ac.jp>
2023-06-09 18:20:22 +0900

    Won/lose message added

--- DiceGame.py
+++ DiceGame.py
@@ -6,4 +6,7 @@ dices = [r.randint(1,6) for i in range(2)]
 for i in range(1,3):
     print(f"Die {i}: {dices[i-1]}")
 else:
-    print(f"Total value: {sum(dices)}")
+    num = sum(dices)
+    print(f"Total value: {num}")
+
+print("You won") if num > 7 else print("You lose")
